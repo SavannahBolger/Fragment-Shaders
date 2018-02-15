e062c1d036ced6078e3116ef8dfd3bb6e8189acb
(Student) - Savannah Bolger <C00215747@ITCARLOW.IE>
2018-02-15 12:51:38 +0000

    points

--- 3D Cube Shader/SFMLOpenGL/Game.cpp
+++ 3D Cube Shader/SFMLOpenGL/Game.cpp
@@ -40,8 +40,8 @@ typedef struct
 	float color[4];
 } Vertex;
 
-Vertex vertex[3];
-GLubyte triangles[3];
+Vertex vertex[36];
+GLubyte triangles[6];
 
 /* Variable to hold the VBO identifier and shader data */
 GLuint	index, //Index to draw
@@ -63,36 +63,307 @@ void Game::initialize()
 	glewInit();
 
 	/* Vertices counter-clockwise winding */
-	vertex[0].coordinate[0] = -0.5f;
-	vertex[0].coordinate[1] = -0.5f;
+	vertex[0].coordinate[0] = -0.2f;
+	vertex[0].coordinate[1] = -0.2f;
 	vertex[0].coordinate[2] = 0.0f;
 
-	vertex[1].coordinate[0] = -0.5f;
-	vertex[1].coordinate[1] = 0.5f;
+	vertex[1].coordinate[0] = -0.2f;
+	vertex[1].coordinate[1] = 0.2f;
 	vertex[1].coordinate[2] = 0.0f;
 
-	vertex[2].coordinate[0] = 0.5f;
-	vertex[2].coordinate[1] = 0.5f;
+	vertex[2].coordinate[0] = 0.2f;
+	vertex[2].coordinate[1] = 0.2f;
 	vertex[2].coordinate[2] = 0.0f;
 
-	vertex[0].color[0] = 0.0f;
-	vertex[0].color[1] = 0.0f;
+	vertex[3].coordinate[0] = 0.2f;
+	vertex[3].coordinate[1] = 0.2f;
+	vertex[3].coordinate[2] = 0.0f;
+
+	vertex[4].coordinate[0] = 0.2f;
+	vertex[4].coordinate[1] = -0.2f;
+	vertex[4].coordinate[2] = 0.0f;
+
+	vertex[5].coordinate[0] = -0.2f;
+	vertex[5].coordinate[1] = -0.2f;
+	vertex[5].coordinate[2] = 0.0f;
+
+	vertex[6].coordinate[0] = 0.2f;
+	vertex[6].coordinate[1] = -0.2f;
+	vertex[6].coordinate[2] = 0.0f;
+
+	vertex[7].coordinate[0] = 0.2f;
+	vertex[7].coordinate[1] = 0.2f;
+	vertex[7].coordinate[2] = 0.0f;
+
+	vertex[8].coordinate[0] = 0.2f;
+	vertex[8].coordinate[1] = 0.2f;
+	vertex[8].coordinate[2] = 0.2f;
+
+	vertex[9].coordinate[0] = 0.2f;
+	vertex[9].coordinate[1] = 0.2f;
+	vertex[9].coordinate[2] = 0.2f;
+
+	vertex[10].coordinate[0] = 0.2f;
+	vertex[10].coordinate[1] = -0.2f;
+	vertex[10].coordinate[2] = 0.2f;
+
+	vertex[11].coordinate[0] = 0.2f;
+	vertex[11].coordinate[1] = -0.2f;
+	vertex[11].coordinate[2] = 0.0f;
+
+	vertex[12].coordinate[0] = 0.2f;
+	vertex[12].coordinate[1] = -0.2f;
+	vertex[12].coordinate[2] = 0.2f;
+
+	vertex[13].coordinate[0] = 0.2f;
+	vertex[13].coordinate[1] = 0.2f;
+	vertex[13].coordinate[2] = 0.2f;
+
+	vertex[14].coordinate[0] = -0.2f;
+	vertex[14].coordinate[1] = 0.2f;
+	vertex[14].coordinate[2] = 0.2f;
+
+	vertex[15].coordinate[0] = -0.2f;
+	vertex[15].coordinate[1] = 0.2f;
+	vertex[15].coordinate[2] = 0.2f;
+
+	vertex[16].coordinate[0] = -0.2f;
+	vertex[16].coordinate[1] = -0.2f;
+	vertex[16].coordinate[2] = 0.2f;
+
+	vertex[17].coordinate[0] = 0.2f;
+	vertex[17].coordinate[1] = -0.2f;
+	vertex[17].coordinate[2] = 0.2f;
+
+	vertex[18].coordinate[0] = -0.2f;
+	vertex[18].coordinate[1] = -0.2f;
+	vertex[18].coordinate[2] = 0.2f;
+
+	vertex[19].coordinate[0] = -0.2f;
+	vertex[19].coordinate[1] = 0.2f;
+	vertex[19].coordinate[2] = 0.2f;
+
+	vertex[20].coordinate[0] = -0.2f;
+	vertex[20].coordinate[1] = 0.2f;
+	vertex[20].coordinate[2] = 0.0f;
+
+	vertex[21].coordinate[0] = -0.2f;
+	vertex[21].coordinate[1] = 0.2f;
+	vertex[21].coordinate[2] = 0.0f;
+
+	vertex[22].coordinate[0] = -0.2f;
+	vertex[22].coordinate[1] = -0.2f;
+	vertex[22].coordinate[2] = 0.0f;
+
+	vertex[23].coordinate[0] = -0.2f;
+	vertex[23].coordinate[1] = -0.2f;
+	vertex[23].coordinate[2] = 0.2f;
+
+	vertex[24].coordinate[0] = -0.2f;
+	vertex[24].coordinate[1] = 0.2f;
+	vertex[24].coordinate[2] = 0.0f;
+
+	vertex[25].coordinate[0] = -0.2f;
+	vertex[25].coordinate[1] = 0.2f;
+	vertex[25].coordinate[2] = 0.2f;
+
+	vertex[26].coordinate[0] = 0.2f;
+	vertex[26].coordinate[1] = 0.2f;
+	vertex[26].coordinate[2] = 0.2f;
+
+	vertex[27].coordinate[0] = 0.2f;
+	vertex[27].coordinate[1] = 0.2f;
+	vertex[27].coordinate[2] = 0.2f;
+
+	vertex[28].coordinate[0] = 0.2f;
+	vertex[28].coordinate[1] = 0.2f;
+	vertex[28].coordinate[2] = 0.0f;
+
+	vertex[29].coordinate[0] = -0.2f;
+	vertex[29].coordinate[1] = 0.2f;
+	vertex[29].coordinate[2] = 0.0f;
+
+	vertex[30].coordinate[0] = -0.2f;
+	vertex[30].coordinate[1] = -0.2f;
+	vertex[30].coordinate[2] = 0.2f;
+
+	vertex[31].coordinate[0] = -0.2f;
+	vertex[31].coordinate[1] = -0.2f;
+	vertex[31].coordinate[2] = 0.0f;
+
+	vertex[32].coordinate[0] = 0.2f;
+	vertex[32].coordinate[1] = -0.2f;
+	vertex[32].coordinate[2] = 0.0f;
+
+	vertex[33].coordinate[0] = 0.2f;
+	vertex[33].coordinate[1] = -0.2f;
+	vertex[33].coordinate[2] = 0.0f;
+
+	vertex[34].coordinate[0] = 0.2f;
+	vertex[34].coordinate[1] = -0.2f;
+	vertex[34].coordinate[2] = 0.2f;
+
+	vertex[35].coordinate[0] = -0.2f;
+	vertex[35].coordinate[1] = -0.2f;
+	vertex[35].coordinate[2] = 0.2f;
+
+	vertex[0].color[0] = 0.1f;
+	vertex[0].color[1] = 1.0f;
 	vertex[0].color[2] = 0.0f;
-	vertex[0].color[3] = 1.0f;
 
-	vertex[1].color[0] = 0.0f;
-	vertex[1].color[1] = 0.0f;
+	vertex[1].color[0] = 0.2f;
+	vertex[1].color[1] = 1.0f;
 	vertex[1].color[2] = 0.0f;
-	vertex[1].color[3] = 1.0f;
 
-	vertex[2].color[0] = 0.0f;
-	vertex[2].color[1] = 0.0f;
+	vertex[2].color[0] = 0.3f;
+	vertex[2].color[1] = 1.0f;
 	vertex[2].color[2] = 0.0f;
-	vertex[2].color[3] = 1.0f;
+
+	vertex[3].color[0] = 0.4f;
+	vertex[3].color[1] = 1.0f;
+	vertex[3].color[2] = 0.0f;
+
+	vertex[4].color[0] = 0.5f;
+	vertex[4].color[1] = 1.0f;
+	vertex[4].color[2] = 0.0f;
+
+	vertex[5].color[0] = 0.6f;
+	vertex[5].color[1] = 1.0f;
+	vertex[5].color[2] = 0.0f;
+
+	vertex[6].color[0] = 0.7f;
+	vertex[6].color[1] = 1.0f;
+	vertex[6].color[2] = 0.0f;
+
+	vertex[7].color[0] = 0.8f;
+	vertex[7].color[1] = 1.0f;
+	vertex[7].color[2] = 0.0f;
+
+	vertex[8].color[0] = 0.9f;
+	vertex[8].color[1] = 1.0f;
+	vertex[8].color[2] = 0.0f;
+
+	vertex[9].color[0] = 1.0f;
+	vertex[9].color[1] = 1.0f;
+	vertex[9].color[2] = 0.0f;
+
+	vertex[10].color[0] = 1.1f;
+	vertex[10].color[1] = 1.0f;
+	vertex[10].color[2] = 0.0f;
+
+	vertex[11].color[0] = 1.2f;
+	vertex[11].color[1] = 1.0f;
+	vertex[11].color[2] = 0.0f;
+
+	vertex[12].color[0] = 1.3f;
+	vertex[12].color[1] = 1.0f;
+	vertex[12].color[2] = 0.0f;
+
+	vertex[13].color[0] = 1.4f;
+	vertex[13].color[1] = 1.0f;
+	vertex[13].color[2] = 0.0f;
+
+	vertex[14].color[0] = 1.5f;
+	vertex[14].color[1] = 1.0f;
+	vertex[14].color[2] = 0.0f;
+
+	vertex[15].color[0] = 1.6f;
+	vertex[15].color[1] = 1.0f;
+	vertex[15].color[2] = 0.0f;
+
+	vertex[16].color[0] = 1.7f;
+	vertex[16].color[1] = 1.0f;
+	vertex[16].color[2] = 0.0f;
+
+	vertex[17].color[0] = 1.8f;
+	vertex[17].color[1] = 1.0f;
+	vertex[17].color[2] = 0.0f;
+
+	vertex[18].color[0] = 1.9f;
+	vertex[18].color[1] = 1.0f;
+	vertex[18].color[2] = 0.0f;
+
+	vertex[19].color[0] = 2.0f;
+	vertex[19].color[1] = 1.0f;
+	vertex[19].color[2] = 0.0f;
+
+	vertex[20].color[0] = 2.1f;
+	vertex[20].color[1] = 1.0f;
+	vertex[20].color[2] = 0.0f;
+
+	vertex[21].color[0] = 2.2f;
+	vertex[21].color[1] = 1.0f;
+	vertex[21].color[2] = 0.0f;
+
+	vertex[22].color[0] = 2.3f;
+	vertex[22].color[1] = 1.0f;
+	vertex[22].color[2] = 0.0f;
+
+	vertex[23].color[0] = 2.4f;
+	vertex[23].color[1] = 1.0f;
+	vertex[23].color[2] = 0.0f;
+
+	vertex[24].color[0] = 2.5f;
+	vertex[24].color[1] = 1.0f;
+	vertex[24].color[2] = 0.0f;
+
+	vertex[25].color[0] = 2.6f;
+	vertex[25].color[1] = 1.0f;
+	vertex[25].color[2] = 0.0f;
+
+	vertex[26].color[0] = 2.7f;
+	vertex[26].color[1] = 1.0f;
+	vertex[26].color[2] = 0.0f;
+
+	vertex[27].color[0] = 2.8f;
+	vertex[27].color[1] = 1.0f;
+	vertex[27].color[2] = 0.0f;
+
+	vertex[28].color[0] = 2.9f;
+	vertex[28].color[1] = 1.0f;
+	vertex[28].color[2] = 0.0f;
+
+	vertex[29].color[0] = 3.0f;
+	vertex[29].color[1] = 1.0f;
+	vertex[29].color[2] = 0.0f;
+
+	vertex[30].color[0] = 3.1f;
+	vertex[30].color[1] = 1.0f;
+	vertex[30].color[2] = 0.0f;
+
+	vertex[31].color[0] = 3.2f;
+	vertex[31].color[1] = 1.0f;
+	vertex[31].color[2] = 0.0f;
+
+	vertex[32].color[0] = 3.3f;
+	vertex[32].color[1] = 1.0f;
+	vertex[32].color[2] = 0.0f;
+
+	vertex[33].color[0] = 3.4f;
+	vertex[33].color[1] = 1.0f;
+	vertex[33].color[2] = 0.0f;
+
+	vertex[34].color[0] = 3.5f;
+	vertex[34].color[1] = 1.0f;
+	vertex[34].color[2] = 0.0f;
+
+	vertex[35].color[0] = 3.6f;
+	vertex[35].color[1] = 1.0f;
+	vertex[35].color[2] = 0.0f;
 
 	/*Index of Poly / Triangle to Draw */
 	triangles[0] = 0;   triangles[1] = 1;   triangles[2] = 2;
-
+	triangles[3] = 3;   triangles[4] = 4;   triangles[5] = 5;
+	triangles[6] = 6;   triangles[7] = 7;   triangles[8] = 8;
+	triangles[9] = 9;   triangles[10] = 10;   triangles[11] = 11;
+	triangles[12] = 12;   triangles[13] = 13;   triangles[14] = 14;
+	triangles[15] = 15;   triangles[16] = 16;   triangles[17] = 17;
+	triangles[18] = 18;   triangles[19] = 19;   triangles[20] = 20;
+	triangles[21] = 21;   triangles[22] = 22;   triangles[23] = 23;
+	triangles[24] = 24;   triangles[25] = 25;	  triangles[26] = 26;
+	triangles[27] = 27;   triangles[28] = 28;	  triangles[29] = 29;
+	triangles[30] = 30;   triangles[31] = 31;   triangles[32] = 32;
+	triangles[33] = 33;	 triangles[34] = 34;   triangles[35] = 35;
 	/* Create a new VBO using VBO id */
 	glGenBuffers(1, vbo);
 
@@ -105,7 +376,7 @@ void Game::initialize()
 
 	glGenBuffers(1, &index);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLubyte) * 3, triangles, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLubyte) * 36, triangles, GL_STATIC_DRAW);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	/* Vertex Shader which would normally be loaded from an external file */
@@ -263,7 +534,7 @@ void Game::render()
 	glEnableVertexAttribArray(positionID);
 	glEnableVertexAttribArray(colorID);
 
-	glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_BYTE, (char*)NULL + 0);
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_BYTE, (char*)NULL + 0);
 
 	window.display();
 
